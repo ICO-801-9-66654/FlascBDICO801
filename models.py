2fec53a22980f265566d0710f0ef2328f115065f
Enzo Ferreira <enzosebastian75@gmail.com>
2026-03-12 08:46:58 -0600

    Avance Alumnos

--- models.py
+++ models.py
@@ -1,8 +1,7 @@
-from flask_sqlachemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 import datetime
 
 db = SQLAlchemy()
-
 class Alumnos(db.Model):
     __tablename__ = 'alumnos'
     id = db.Column(db.Integer, primary_key=True)
@@ -10,4 +9,4 @@ class Alumnos(db.Model):
     apaterno = db.Column(db.String(50), nullable=False)
     amaterno = db.Column(db.String(50), nullable=False)
     edad = db.Column(db.Integer, nullable=False)
-    correo = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100),  nullable=False)
